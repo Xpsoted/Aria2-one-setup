b1ff3b11806d13aa8e80c3e3c6c98a6be3438422
“Your <you@example.com>
2020-07-01 10:51:49 +0800

    Signed-off-by: “Your <“you@example.com”>

---
+++ start.bat
@@ -0,0 +1,16 @@
+@echo off
+mode con:cols=70 lines=30
+title=Aria2下载器
+color 9F
+echo.
+echo============================================================
+echo.
+echo                  Windows系统Aria2下载器
+echo.
+echo         下载时请勿关闭窗口  关闭窗口即代表结束程序
+echo.
+echo                  神韵编写脚本，XP进行整合
+echo.
+echo============================================================
+
+aria2c.exe --conf=aria2.conf
